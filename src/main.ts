7a8f25e1c240acff8cbaa3e2a8c31d7bd7ac48a6
lcds90 <lcds90@gmail.com>
2022-04-22 04:14:12 -0300

    added i18n

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,11 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import App from './App.vue';
 import router from './router';
+import i18n from './i18n';
 
 const store = createPinia();
 
-createApp(App)
+createApp(App).use(i18n)
   .use(router)
   .use(store)
   .mount('#app');
